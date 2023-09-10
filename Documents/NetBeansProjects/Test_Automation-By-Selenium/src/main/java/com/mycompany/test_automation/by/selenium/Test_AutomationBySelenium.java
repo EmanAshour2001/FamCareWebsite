b84d4403c050dc15bdd599ccf55b600d82189fdf
Eman Ahmed <eman.ah.ash@gmail.com>
2023-09-10 15:50:44 +0300

    Test

---
+++ Documents/NetBeansProjects/Test_Automation-By-Selenium/src/main/java/com/mycompany/test_automation/by/selenium/Test_AutomationBySelenium.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test_automation.by.selenium;
+
+/**
+ *
+ * @author HP
+ */
+public class Test_AutomationBySelenium {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
